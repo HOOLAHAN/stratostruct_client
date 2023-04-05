aa4f211469d1e5079a3d9cff519b288c5f5afc3d
Iain <iain.hoolahan@gmail.com>
2023-04-05 16:13:04 +0100

    AuthContext added with custom hook

--- src/index.js
+++ src/index.js
@@ -4,14 +4,17 @@ import './index.css';
 import App from './App';
 import { SuppliersContextProvider } from './context/SuppliersContext';
 import { ProductsContextProvider } from './context/ProductsContext';
+import { AuthContextProvider } from './context/AuthContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SuppliersContextProvider>
-    <ProductsContextProvider>
-      <App />
-    </ProductsContextProvider>
-    </SuppliersContextProvider>
+    <AuthContextProvider>
+      <SuppliersContextProvider>
+        <ProductsContextProvider>
+          <App />
+        </ProductsContextProvider>
+      </SuppliersContextProvider>
+    </AuthContextProvider>
   </React.StrictMode>
 );
