522c8856d4f235e39213de2f7b361d4562756c50
Iain <iain.hoolahan@gmail.com>
2024-01-06 17:20:40 +0000

    test for calculateDistance returning distance with valid inputs passing

---
+++ testing/functions.test.js
@@ -0,0 +1,28 @@
+import calculateDistance from '../src/functions/calculateDistance';
+global.fetch = jest.fn(); // Mocking fetch
+
+// Mocking the environment variable
+process.env.REACT_APP_BACKEND_API_URL = 'http://localhost';
+
+beforeEach(() => {
+    fetch.mockClear();
+});
+
+test('calculateDistance returns distance in kilometers for valid postcodes', async () => {
+    fetch.mockResolvedValueOnce({
+        json: () => Promise.resolve({ distance: 100 }),
+    });
+
+    const distance = await calculateDistance('postcode1', 'postcode2', 'token');
+    expect(distance).toBe('100 km');
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(
+        'http://localhost/api/mapbox/getDistance?postcode1=postcode1&postcode2=postcode2',
+        {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer token',
+            }
+        }
+    );
+});
