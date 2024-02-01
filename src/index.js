fcade4a14235027c05bcb2328e2c1f6506143c5a
Iain <iain.hoolahan@gmail.com>
2024-02-01 18:36:03 +0000

    imported chakra ui package and added wrapper

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { ChakraProvider } from '@chakra-ui/react'
 import { SuppliersContextProvider } from './context/SuppliersContext';
 import { ProductsContextProvider } from './context/ProductsContext';
 import { AuthContextProvider } from './context/AuthContext';
@@ -9,12 +10,14 @@ import { AuthContextProvider } from './context/AuthContext';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <SuppliersContextProvider>
-        <ProductsContextProvider>
-          <App />
-        </ProductsContextProvider>
-      </SuppliersContextProvider>
-    </AuthContextProvider>
+    <ChakraProvider>
+      <AuthContextProvider>
+        <SuppliersContextProvider>
+          <ProductsContextProvider>
+            <App />
+          </ProductsContextProvider>
+        </SuppliersContextProvider>
+      </AuthContextProvider>
+    </ChakraProvider>
   </React.StrictMode>
 );
