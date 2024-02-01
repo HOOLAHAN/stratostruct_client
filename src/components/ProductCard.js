c07f7016b570768d07d6b338af8f361f16473d22
Iain <iain.hoolahan@gmail.com>
2024-02-01 20:40:42 +0000

    convet ProductCard to chakra UI

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Button, Text, Box } from '@chakra-ui/react';
+
 const ProductCard = ({ product, cart, onAddToCart, onRemoveFromCart }) => {
   const isInCart = cart.find((item) => item._id === product._id);
 
@@ -10,16 +13,21 @@ const ProductCard = ({ product, cart, onAddToCart, onRemoveFromCart }) => {
   };
 
   return (
-    <button
-      className={`product-card ${isInCart ? 'selected' : ''}`}
+    <Button
       onClick={handleClick}
-      style={{ background: isInCart ? '#DEFFF2' : 'none' }}
+      bg={isInCart ? '#DEFFF2' : 'transparent'}
+      _hover={{ bg: isInCart ? '#bdf3e5' : 'gray.100' }}
+      width="full"
+      justifyContent="flex-start"
+      borderRadius="md"
+      boxShadow="md"
     >
-      <p style={{ display: 'inline-block', marginLeft: '5px' }}>
-        <strong>{product.component_name}</strong>
-      </p>
-      <br />
-    </button>
+      <Box p="4">
+        <Text fontWeight="bold" display="inline-block" ml="2">
+          {product.component_name}
+        </Text>
+      </Box>
+    </Button>
   );
 };
 
