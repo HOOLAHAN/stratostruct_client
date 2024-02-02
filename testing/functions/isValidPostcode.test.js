d2e496c111231f9a77c44c87496bbd9e17020d26
Iain Hoolahan <iainhoolahan@outlook.com>
2024-02-02 09:44:52 +0000

    updated postcode validator so allow no space case

--- testing/functions/isValidPostcode.test.js
+++ testing/functions/isValidPostcode.test.js
@@ -6,6 +6,11 @@ describe('Postcode validation', () => {
     expect(isValidPostcode('EC1A 1BB')).toBeTruthy();
   });
 
+  test('Validates if postcode has no space or if letters are lower case', () => {
+    expect(isValidPostcode('EC1A1BB')).toBeTruthy();
+    expect(isValidPostcode('ec1a1bb')).toBeTruthy();
+  });
+
   test('Returns false for incorrect postcode format', () => {
     expect(isValidPostcode('12345')).toBeFalsy();
     expect(isValidPostcode('ABCDE')).toBeFalsy();
